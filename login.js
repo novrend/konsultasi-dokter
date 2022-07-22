bb4147b5a3e174929b4e41e76bf8fe35dac7a87c
ihza <ihzan0@gmail.com>
2022-07-22 13:44:36 +0700

    Finishh

--- login.js
+++ login.js
@@ -1,16 +1,19 @@
 function login() {
     let email = document.getElementById('username').value
     let password = document.getElementById('password').value
+    let status = false
     for (let key of logins) {
         if (key[0] === email && key[1] === password) {
             localStorage.setItem('login', key[0])
+            status = true
             break
-        } else {
-            document.getElementById('status-login').innerHTML = "Email/Password Salah!"
-            setTimeout(function () {
-                document.getElementById('status-login').innerHTML = ''
-            }, 1000)
         }
+    }
+    if (!status) {
+        document.getElementById('status-login').innerHTML = "Email/Password Salah!"
+        setTimeout(function () {
+            document.getElementById('status-login').innerHTML = ''
+        }, 1000)
     }
     checks()
 }
